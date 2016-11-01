955e30d287ec7ae591c24c6eaa434ca9722c58a7
genchansansan <what.you.and.i.both.love@gmail.com>
2016-11-01 00:45:23 -0700

    fixed connectNodes.py

--- python2.7libs/connectNodes.py
+++ python2.7libs/connectNodes.py
@@ -1,16 +1,18 @@
 import hou
 import math
 
-limit = len(hou.selectedNodes())
-yPos = []
 
-for aNode in hou.selectedNodes():
-    yPos.append([aNode.position()[1], aNode])
-    yPos = sorted(yPos)
+def execute():
+    limit = len(hou.selectedNodes())
+    yPos = []
 
-for i in xrange(len(yPos) - 1):
-    yPos[i][1].setInput(0,yPos[i+1][1])
+    for aNode in hou.selectedNodes():
+        yPos.append([aNode.position()[1], aNode])
+        yPos = sorted(yPos)
 
-for yNode in hou.selectedNodes():
-    yFit = math.floor(yNode.position()[0])
-    mean = (yNode.position()[1]) - (yFit) / (limit)
+    for i in xrange(len(yPos) - 1):
+        yPos[i][1].setInput(0,yPos[i+1][1])
+
+    for yNode in hou.selectedNodes():
+        yFit = math.floor(yNode.position()[0])
+        mean = (yNode.position()[1]) - (yFit) / (limit)
