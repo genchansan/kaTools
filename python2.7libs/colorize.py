c3248e5549ff2da9ba26dacb15a09493f3356895
genchansansan <what.you.and.i.both.love@gmail.com>
2016-11-01 18:25:22 -0700

    Update colorize.py
    change colors based on name

--- python2.7libs/colorize.py
+++ python2.7libs/colorize.py
@@ -7,6 +7,10 @@ def execute():
     vopClr = hou.Color((1,1,0.6))
     objMergeClr = hou.Color((0,0.6,1))
     nullClr = hou.Color((0.6,0.6,1))
+    subnetClr = hou.Color((0.867,0,0))
+    geoClr = hou.Color((0,0.267,0))
+    geoFxClr = hou.Color((0.4,1,0.4))
+    geoRenClr = hou.Color((0.8,1,0.8))
     
     #######################
     currentNode = hou.selectedNodes()
@@ -27,4 +31,18 @@ def execute():
             node.setColor(dopClr)
         elif node.type().name() == "object_merge":
             node.setColor(objMergeClr)
-        elif node.type().name() =
+        elif node.type().name() == "null":
+            node.setColor(nullClr)
+        elif node.type().name() == "attibvop":
+            node.setColor(vopClr)
+        elif node.type().name() == "avolumevop":
+            node.setColor(vopClr)
+        elif node.type().name() == "subnet":
+            node.setColor(subnetClr)
+        elif node.type().name() == "geo":
+            if node.name().find("FX") != -1:
+                node.setColor(geoFxClr)
+            elif node.name().find("REN") != -1:
+                node.setColor(geoRenClr)
+            else:
+                node.setColor(geoClr)
