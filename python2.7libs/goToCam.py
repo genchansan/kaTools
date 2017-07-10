73db39fdd0b0d349710f5cde9adff72a6d364c0e
genchansansan <what.you.and.i.both.love@gmail.com>
2017-07-10 10:42:24 -0700

    Update goToCam.py
    add al ver

--- python2.7libs/goToCam.py
+++ python2.7libs/goToCam.py
@@ -3,3 +3,20 @@ import hou
 def execute():
     viewports = hou.ui.paneTabOfType(hou.paneTabType.SceneViewer)
     viewports.curViewport().setCamera(hou.node("/obj/renderCamera"))
+
+
+
+def executeAL():
+    viewports = hou.ui.paneTabOfType(hou.paneTabType.SceneViewer)
+    cam = find()
+    if cam is not None:
+        viewports.curViewport().setCamera(cam)
+
+def find():
+    obj = hou.node("/obj")
+    camObj = obj.glob("*camera*")
+    cam = None
+    if camObj != None:
+		cam = hou.node(camObj[0].path() + "/left")
+
+    return cam
