995c178761e46c6e7c6f8132a81d99499453d4f8
genchansansan <what.you.and.i.both.love@gmail.com>
2017-07-11 01:43:40 -0700

    updating xml reader

---
+++ python2.7libs/addExpression.py
@@ -0,0 +1,28 @@
+import hou
+
+def paste(kwargs):
+
+    tree = ET.parse(hou.expandString("$HOUDINI_PATH").split(";")[0] + '/expressions.xml')
+    root = tree.getroot()
+    expression = root.find("./set[@name='" + kwargs["selectedlabel"] +"']").find('expression').text
+
+    #print kwargs["selectedtoken"]
+    #print kwargs["selectedlabel"]
+    #print kwargs["parms"][0]
+
+    return expression
+
+
+###########################################################################
+
+import xml.etree.ElementTree as ET
+
+def readXML():
+    tree = ET.parse(hou.expandString("$HOUDINI_PATH").split(";")[0] + '/expressions.xml')
+    root = tree.getroot()
+    menus = []
+    for menuset in root:
+        menus.append(menuset.tag)
+        menus.append(menuset.attrib[("name")])
+
+    return menus
