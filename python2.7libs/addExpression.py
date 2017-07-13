eaf620ee9a617d009b30f59885f68759ea6b1b0c
genchansansan <what.you.and.i.both.love@gmail.com>
2017-07-12 19:06:06 -0700

    Update addExpression.py
    adding expat

--- python2.7libs/addExpression.py
+++ python2.7libs/addExpression.py
@@ -1,30 +1,45 @@
 # -*- coding: utf-8 -*-
 import hou
 import xml.etree.ElementTree as ET
+import sys
+import xml.parsers.expat as ep
 
 class wranglePreset:
-    def __init__(self):
-        self.XMLPath = hou.expandString("$HOUDINI_PATH").split(";")[-2] + '/expressions.xml'
-        self.tree = ET.parse(XMLPath)
+
+	parser = ""
+	menus = []
+
+	def __init__(self):
+		self.XMLPath = hou.expandString("$HOUDINI_PATH").split(";")[-2] + '/expressions.xml'
+		self.tree = ET.parse(self.XMLPath)
+
+		self.parser = ep.ParserCreate("UTF-8")
+		self.parser.CharacterDataHandler = self.handleCharData
+		self.parser.StartElementHandler = self.handleStartElement
+		self.parser.EndElementHandler = self.handleEndElement
 
 
-        
 
-    def paste(kwargs):
+	def handleCharData(self, data):
+		print 'Character data:', data
+	def handleStartElement(self, name, attrs):
+		if name == "set":
+			self.menus.append(attrs["name"])
+			print 'Start element:', name, attrs
+	def handleEndElement(self, name):
+		print 'End element:', name
 
-	#self.tree = ET.parse(hou.expandString("$HOUDINI_PATH").split(";")[-2] + '/expressions.xml')
-	root = self.tree.getroot()
-	expression = root.find("./set[@name='" + kwargs["selectedlabel"] +"']").find('expression').text
 
-	#print kwargs["selectedtoken"]
-	#print kwargs["selectedlabel"]
-	#print kwargs["parms"][0]
+	def paste(self,kwargs):
+		root = self.tree.getroot()
+		expression = root.find("./set[@name='" + kwargs["selectedlabel"] +"']").find('expression').text
 
-	print kwargs
+		self.parser.ParseFile(open(self.XMLPath, "r"))
+		print self.menus
 
-	kwargs["parms"][0].set(kwargs["parms"][0].eval() + expression)
+		kwargs["parms"][0].set(kwargs["parms"][0].eval() + expression)
 
-	return expression
+		return expression
 
 
 
@@ -32,32 +47,29 @@ class wranglePreset:
 
 
 
-    def readXML():
-	#tree = ET.parse(hou.expandString("$HOUDINI_PATH").split(";")[-2] + '/expressions.xml')
-	root = self.tree.getroot()
-	menus = []
-	for menuset in root:
-	    menus.append(menuset.tag)
-	    menus.append(menuset.attrib[("name")])
+	def readXML(self):
+		root = self.tree.getroot()
+		menus = []
+		for menuset in root:
+			menus.append(menuset.attrib[("name")])
+			menus.append(menuset.attrib[("name")])
 
-	return menus
-
-
-    def saveXML(kwargs):
-	#XMLPath = hou.expandString("$HOUDINI_PATH").split(";")[-2] + '/expressions.xml'
-	#tree = ET.parse(XMLPath)
-	root = self.tree.getroot()
-
-	accept, name = hou.ui.readInput("Preset Name:")
-	expression = kwargs["parms"][0].eval()
-
-	setElement = ET.Element("set")
-	setElement.set("name", name)
-	expressionElement = ET.Element("expression")
-	expressionElement.text = expression
-	root.append(setElement)
-	setElement.append(expressionElement)
+		return menus
 
 
-	tree.write(XMLPath, "UTF-8", True)
-    
+	def saveXML(self,kwargs):
+		root = self.tree.getroot()
+
+		accept, name = hou.ui.readInput("Preset Name:")
+		expression = kwargs["parms"][0].eval()
+
+		setElement = ET.Element("set")
+		setElement.set("name", name)
+		expressionElement = ET.Element("expression")
+		expressionElement.text = expression
+		root.append(setElement)
+		setElement.append(expressionElement)
+
+
+		tree.write(XMLPath, "UTF-8", True)
+
