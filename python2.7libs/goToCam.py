d14900c9e039039a446331c5532ac2ca2c815b98
Unknown <what.you.and.i.both.love@gmail.com>
2018-08-21 00:06:44 -0700

    addin pyro clastic example

--- python2.7libs/goToCam.py
+++ python2.7libs/goToCam.py
@@ -11,12 +11,28 @@ def executeAL():
     cam = find()
     if cam is not None:
         viewports.curViewport().setCamera(cam)
+    else:
+        cam = findOther()
+        if cam is not None:
+            viewports.curViewport().setCamera(cam)
+
+
 
 def find():
     obj = hou.node("/obj")
     camObj = obj.glob("*camera*")
     cam = None
-    if camObj != None:
+    if len(camObj)>0:
 		cam = hou.node(camObj[0].path() + "/left")
+
+    return cam
+
+def findOther():
+    obj = hou.node("/obj")
+    camObj = obj.recursiveGlob("*", hou.nodeTypeFilter.ObjCamera)
+
+    cam = None
+    if camObj != None:
+		cam = camObj[0]
 
     return cam
